faa4982fbd059b5283f5fe4ab4c2e0dfd7660bb9
Mihai Corocaescu <mihai.corocaescu2@gmail.com>
2023-03-13 14:59:50 +0200

    repo fix

--- Ubb.BikeContest.Model/src/RaceEntry.cs
+++ Ubb.BikeContest.Model/src/RaceEntry.cs
@@ -2,17 +2,16 @@
 
 public class RaceEntry : Identifiable<long>
 {
-    private readonly Participant _participant;
     private readonly Race _race;
 
     public RaceEntry(long id, Participant participant, Race race)
     {
         Id = id;
-        _participant = participant;
+        Participant = participant;
         _race = race;
     }
 
-    public Participant Participant => _participant;
+    public Participant Participant { get; }
 
     public Race Race => _race;
 }
